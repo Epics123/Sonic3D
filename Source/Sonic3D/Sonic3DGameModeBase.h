4565323d353cf98c23acdffb6f80801017738f9f
Ethan Heil <eheil20@gmail.com>
2020-11-04 23:30:08 -0500

    Adding base project

---
+++ Source/Sonic3D/Sonic3DGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "Sonic3DGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SONIC3D_API ASonic3DGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
